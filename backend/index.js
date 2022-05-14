b53be79ea1bd7e3cf328a4e6151b80dc70a996d2
lukasunara <lukasunara7@gmail.com>
2022-05-14 11:14:32 +0200

    Add files for all routes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,35 @@
 const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-// const userRoutes = require('./routes/User.js');
+const scheduleRoutes = require('./routes/scheduleRoutes');
+const ferryRoutes = require('./routes/ferryRoutes');
+const countryRoutes = require('./routes/countryRoutes');
+const tripRoutes = require('./routes/tripRoutes');
+const cityRoutes = require('./routes/cityRoutes');
+const registeredUserRoutes = require('./routes/registeredUserRoutes');
+const travelCompanyRoutes = require('./routes/travelCompanyRoutes');
+const travelerRoutes = require('./routes/travelerRoutes');
+const ferryRouteRoutes = require('./routes/ferryRouteRoutes');
+const ticketRoutes = require('./routes/ticketRoutes');
+const tripRouteRoutes = require('./routes/tripRouteRoutes');
 
 app.use(cors());
-app.use(bodyParser.json({ limit: "24mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "24mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// app.use('/user', userRoutes)
+app.use('/schedules', scheduleRoutes)
+app.use('/ferries', ferryRoutes)
+app.use('/countries', countryRoutes)
+app.use('/trips', tripRoutes)
+app.use('/cities', cityRoutes)
+app.use('/registeredUsers', registeredUserRoutes)
+app.use('/travelCompanies', travelCompanyRoutes)
+app.use('/travelers', travelerRoutes)
+app.use('/ferryRoutes', ferryRouteRoutes)
+app.use('/ticekts', ticketRoutes)
+app.use('/tripRoutes', tripRouteRoutes)
 
 const PORT = process.env.PORT || 3000;
 
